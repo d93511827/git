61f3d53ad917eb0d94026de5492afb075a2c7d21
wang <d93511827@gmail.com>
2017-01-19 23:40:16 +0800

    106/01/19更新1139pm

---
+++ class/log.md
@@ -0,0 +1,8 @@
+Final: 2017/01/19  
+實作地址排序
+
+新增一個靜態陣列，與靜態地址陣列
+const int arr[]{1,2,3};
+int* arrp[]{arr[0], arr[1], arr[2]};
+開始編譯就出問題如下圖  
+![](https://i.imgur.com/KHbqPcM.png)
--- class/數值排序2.cpp
+++ class/數值排序2.cpp
@@ -4,31 +4,17 @@ Date : 2017/01/19
 By   : Username
 Final: 2017/01/19
 *****************************************************************/
-
 #include <iostream>
 using namespace std;
 /*==============================================================*/
 int main(int argc, char const *argv[]){
-	int b=7,c;
-	int* a=new int[b]{6,2,5,4,8,7,1};
-	for(int i=0;i<b;i++){
-		cout<<a[i]<<",";
-	}
-	cout<<endl;
-	//排序
-	for(int i=1;i<b;i++){
-		for(int j=b-1;j>0;j--){
-			if(a[j]<a[j-1]){
-				c=a[j];
-				a[j]=a[j-1];
-				a[j-1]=c;
-			}
-		}
-	}
+	const int arr[]{1,2,3};
+	int* arrp[]{arr[0], arr[1], arr[2]};
 
-	for(int i=0;i<b;i++){
-		cout<<a[i]<<",";
-	}
-		return 0;
+	
+	
+
+	cout<<&arrp[1];
+	return 0;
 }
 /*==============================================================*/
