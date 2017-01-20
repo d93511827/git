ad4e145fe310e07b73d392a794b063754665d550
wang <d93511827@gmail.com>
2017-01-20 13:22:45 +0800

    第三題完成

--- cpp訓練作業串/readme.md
+++ cpp訓練作業串/readme.md
@@ -1,4 +1,7 @@
 第一題實現結果(如下圖)  
 ![](https://i.imgur.com/14DLEzA.png)  
 第二題實現結果  
-![](https://i.imgur.com/98EJHHM.png)
+![](https://i.imgur.com/98EJHHM.png)  
+第三題實現結果  
+![](https://i.imgur.com/q7ROH2W.png)
+
--- cpp訓練作業串/work3.cpp
+++ cpp訓練作業串/work3.cpp
@@ -8,8 +8,25 @@ Final: 2017/01/20
 using namespace std;
 /*==============================================================*/
 int main(int argc, char const *argv[]){
-	float a=0.123456789,b=0.1111111;
-	cout <<a<<","<<b;
+	float a=0.126,b=0.01;
+	int d,c,e;
+	c=1/b;
+
+	cout <<"x=" << a <<","<<"y="<< b <<endl;
+	//cout << c <<endl;
+	d=a*c;
+	e=a*c*10;
+	cout << d*b<<endl;
+	if((e)%10!=0){
+		cout<<(d+1)*b<<endl;
+	}
+	
+	if((e)%10>4){
+		cout<<(d+1)*b<<endl;
+	}
+	else{
+		cout<<d*b<<endl;
+	}
 	return 0;
 }
 /*==============================================================*/
