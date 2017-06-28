8262deb099294521b0af80d353d51e2ac96ba5de
wang <d93511827@gmail.com>
2017-06-28 14:37:08 +0800

    第十題

---
+++ cpp訓練作業串/work10/work10.hpp
@@ -0,0 +1,20 @@
+/*****************************************************************
+Name : 
+Date : 2017/06/28
+By   : Username
+Final: 2017/06/28
+*****************************************************************/
+#ifndef WORK10_HPP
+#define WORK10_HPP
+class Fraction{
+public:
+	Fraction();
+	~Fraction();
+
+private:
+	int a,b,c;
+	float abc;
+};
+
+
+#endif // WORK10_HPP
